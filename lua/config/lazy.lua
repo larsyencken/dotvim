1accb2cd4bec1258b91952addd166542f250ed5d
Lars Yencken <lars@yencken.org>
2024-05-17 09:21:26 +0100

    Bump lazyvim and check weekly

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -59,7 +59,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true, frequency = 604800 }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
